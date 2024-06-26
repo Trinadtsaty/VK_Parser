847a77421bc2a57915cb75aa837e84bb85b12881
Trinadtsatyy <ni9htwatch@yandex.ru>
2024-06-26 12:07:04 +0300

    1

--- CODE.py
+++ CODE.py
@@ -6,49 +6,89 @@ import datetime
 from datetime import datetime
 
 group_id="footballpremierleague_hse"
+# group_id="ultdank"
 #
-count=20
+count=100
 fields="sex"
 fields_group="activity"
 extended="1"
-
+football=["football","Футбол"]
 
 def user_from_group(group_id, count, token):
+    # Загружаем JSON файл, с пользователями группы
     json_per = []
-    fields = "sex,is_closed,city,bdate"
+    fields = "sex,is_closed,city,bdate,deactivated"
     url_user_group=f"https://api.vk.com/method/groups.getMembers?group_id={group_id}&count={count}&fields={fields}&access_token={token}&v=5.199"
     req = requests.get(url_user_group)
     src = req.json()
     posts = src["response"]["items"]
+
+    # Идёт циклом по каждому пользователю
     for item in posts:
+        # Получаем статус пользователя (отфильтровываем забаненные или удалённые страницы)
         try:
+            banned = item["deactivated"]
+        except:
+            banned = None
+
+        if banned=="banned" or banned=="deleted":
+            continue
+
+        else:
+
+            # Фильтруем женщин
             if item["sex"] == 1:
                 continue
-            else:
-                link = "https://vk.com/id" + str(item["id"])
-                id = item["id"]
-                open_close=item["is_closed"]
-                if open_close:
-                    age = 0
-                    json_per.append({"ID": id, "LINK": link, "CLOSE": open_close, "CITY": "НЕ УКАЗАНО", "AGE": "НЕ УКАЗАНО"})
-                else:
-                    age=str(item["bdate"])
-                    if len(age)>=8:
-                        age=int(age[len(age)-4:])
-                        data_now = int(str(datetime.now())[:4])
-                        age=str(data_now-age)
 
+            else:
+                id = item["id"]
+                link = "https://vk.com/id" + str(id)
+
+                # Проверяем закрытый ли у пользователя профиль, где True - закрытый
+                open_close = item["is_closed"]
+
+                if open_close:
+                    # Записываем json строку, если профиль - закрытый
+                    json_per.append(
+                        {"ID": id, "LINK": link, "CLOSE": open_close, "CITY": "НЕ УКАЗАНО", "AGE": "НЕ УКАЗАНО"}
+                    )
+                else:
+
+                    # получаем даду рождения пользователя
+                    try:
+                        age = str(item["bdate"])
+                    except:
+                        age="НЕ УКАЗАНО"
+
+                    # Проверяем указан ли в дате год рождения, если да, вычисляем сколько лет человеку исполниться или исполнилось в этом году
+                    if (len(age) >= 8) and age!="НЕ УКАЗАНО":
+                        age = int(age[len(age) - 4:])
+                        data_now = int(str(datetime.now())[:4])
+                        age = str(data_now - age)
                     else:
                         age="НЕ УКАЗАНО"
-                    city=item["city"]["title"]
-                    json_per.append({"ID": id, "LINK": link, "CLOSE": open_close, "CITY": city, "AGE": age})
-        except:
-            continue
+
+                    # Узнаём указанный город
+                    try:
+                        city=item["city"]["title"]
+                    except:
+                        city="НЕ УКАЗАНО"
+
+                # Записываем файл в массив с json строками
+                json_per.append({"ID": id, "LINK": link, "CLOSE": open_close, "CITY": city, "AGE": age})
+
+                # Собираем группы, на которые подписан пользователь
+
+
+
     return json_per
+
+
+
 def group_users(user_id, token):
     extended = "1"
     fields_group = "activity"
-    url_group=f"https://api.vk.com/method/users.getSubscriptions?user_id={user_id}&extended={extended}&fields={fields_group}&access_token={token}&v=5.199"
+    url_user_group=f"https://api.vk.com/method/users.getSubscriptions?user_id={user_id}&extended={extended}&fields={fields_group}&access_token={token}&v=5.199"
     req = requests.get(url_user_group)
     src = req.json()
     posts = src["response"]["items"]
--- Chernovik.py
+++ Chernovik.py
@@ -4,10 +4,20 @@ import datetime
 from datetime import datetime
 # with open('people.json') as f:
     # templates = f.read()
-templates=pandas.read_json("people.json")
+# templates=pandas.read_json("people.json")
+# for item in templates:
+#     # if item["banned"] !=None:
+#     print(item.head())
 
-print(templates)
+#
+# print(templates.head())
 
 # data_now = datetime.now()
 # data_now = str(datetime.now())[:4]
 # print(data_now)
+
+with open('people.json') as f:
+    templates = json.load(f)
+
+# for item in templates:
+    # print(item["banned"])
