3bd510c2fbb6e1a6676132b664ee5a572b5e1045
Simon <semen.learning@gmail.com>
2024-06-29 17:24:48 +0300

    add Parser

---
+++ Config.py
@@ -0,0 +1,16 @@
+tokens = {
+    "vk_token" : "todo",
+    "telegram_token" : "7380972075:AAEojzb18qqBOo2PFCLXbzAistjcixpeWIc"
+}
+
+
+find_params = {
+    "fields_groups" : "activity,deactivated,description,is_closed",
+    "group_id" : "footballpremierleague_hse",
+    "fields" : "sex,is_closed,city,bdate,deactivated",
+    "ban_city" : ["Санкт-Петербург"],
+    "football_keyword" : ["Football","Футбол","Football","ФУТБОЛ","FOOTBALL","футбол","football", "ФК", "фк"],
+    "filtre_age" : 1000000,
+    "ban_activity" : "",
+    "name" : "people_open_with_groups.json"
+}
