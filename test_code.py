497d7a20afe33a9bd461ab51d0a45c4f031c254a
Simon <semen.learning@gmail.com>
2024-06-30 14:55:30 +0300

    new command

--- test_code.py
+++ test_code.py
@@ -2,6 +2,7 @@ import telebot
 from Config import tokens
 from CODE import data_parsing
 from add_tok import token
+import pandas as pd 
 # token = tokens['telegram_token']
 token1="5308900440:AAFsI35w_esbY_RgtGmUf3fTbFLUTBphGgk"
 bot = telebot.TeleBot(token1)
@@ -13,6 +14,18 @@ def send_help(message, token):
         data_parsing(group_id, token)
     except:
         bot.send_message(message.chat.id, message.text[5:] + "Не верный ID группы")
+
+@bot.message_handler(commands=["get_data"])
+def get_data(message, token):
+    new_file = "DB/" + date.today().strftime("%d_%m_%Y") + ".json"
+    data = pd.read_json(new_file)
+
+    slice = data[:5]
+
+    slice_str = slice.to_string()
+    bot.send_message(message.chat.id, slice_str)
+
+
 
 @bot.message_handler(commands=["help"])
 def send(message):
