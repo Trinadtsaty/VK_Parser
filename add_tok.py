3abcdf339e3e52008a7756e73c50d3ee62ab1c8d
Trinadtsatyy <ni9htwatch@yandex.ru>
2024-07-03 12:38:23 +0300

    Update add_tok.py

--- add_tok.py
+++ add_tok.py
@@ -1,2 +1,12 @@
-# необходимо вставить токен в переменную "token"
-token=""
+
+
+
+find_params = {
+    "fields_groups" : "activity,deactivated,description,is_closed",
+    "fields" : "sex,is_closed,city,bdate,deactivated",
+    "ban_city" : ["Санкт-Петербург"],
+    "football_keyword" : ["Football","Футбол","Football","ФУТБОЛ","FOOTBALL","футбол","football", "ФК", "фк"],
+    "filtre_age" : 1000000,
+    "ban_activity" : "",
+    "group_mass" : ["222824253"]
+}
